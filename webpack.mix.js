8619b34c3108f9dfb55c1ddf68d6ceb37cd34d77
Ian Travers <mr.itpe@gmail.com>
2019-07-30 15:17:33 +0300

    Switch to laravel-mix versioned assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,8 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('/build/')
+    .js('resources/js/app.js', 'js')
+    .sass('resources/sass/app.scss', 'css')
+    .version();
