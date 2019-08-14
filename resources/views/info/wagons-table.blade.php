9584275c9ab8dbdd1be9bcc7451a2414078d865b
Ian Travers <mr.itpe@gmail.com>
2019-08-14 09:04:15 +0300

    Change root page table

--- resources/views/info/wagons-table.blade.php
+++ resources/views/info/wagons-table.blade.php
@@ -1,34 +1,22 @@
 @php /* @var App\Wagon $wagon */ @endphp
 
-<table class="table table-striped table-bordered">
+<table class="table table-bordered">
   <thead>
   <tr class="text-center text-muted">
     <th>№</th>
     <th>Номер</th>
-    <th>Кем</th>
+    <th>Простой, ч.</th>
     <th>Причина</th>
-    <th>Собственность</th>
-    {{--<th>Ст. отпр.</th>--}}
-    {{--<th>Ст. назн.</th>--}}
-    <th>Прибыл</th>
-    <th>Задержан</th>
-    <th>Принятые меры</th>
   </tr>
   </thead>
   <tbody>
 
   @foreach($wagons as $wagon)
     <tr>
-      <td>{{ $loop->index + 1 }}</td>
-      <td><a href="{{ $wagon->viewPath() }}">{{ $wagon->inw }}</a></td>
-      <td>{{ $wagon->detainer->name }}</td>
+      <td class="text-center">{{ $loop->index + 1 }}</td>
+      <td class="text-center"><a href="{{ $wagon->viewPath() }}">{{ $wagon->inw }}</a></td>
+      <td class="text-right">{{ $wagon->detainedInHours() }}</td>
       <td>{{ $wagon->reason }}</td>
-      <td>{{ $wagon->ownership }}</td>
-{{--      <td>{{ $wagon->departure_station }}</td>--}}
-{{--      <td>{{ $wagon->destination_station }}</td>--}}
-      <td class="text-center">{{ $wagon->arrived_at->format('d.m.Y H:i') }}</td>
-      <td class="text-center">{{ $wagon->detained_at->format('d.m.Y H:i') }}</td>
-      <td>{{ $wagon->taken_measure }}</td>
     </tr>
 
   @endforeach
