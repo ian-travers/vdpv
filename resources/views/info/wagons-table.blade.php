d89ac9986f2cfcf043461b2e716360be832bc23a
Ian Travers <mr.itpe@gmail.com>
2019-08-15 09:12:47 +0300

    Detained in days

--- resources/views/info/wagons-table.blade.php
+++ resources/views/info/wagons-table.blade.php
@@ -5,6 +5,7 @@
   <tr class="text-center text-muted">
     <th width="10%">№</th>
     <th width="15%">Номер</th>
+    <th width="15%">Простой, дн.</th>
     <th width="15%">Простой, ч.</th>
     <th>Причина</th>
   </tr>
@@ -15,6 +16,7 @@
     <tr>
       <td class="text-center">{{ (request('page')) ? (request('page') - 1) * 10 + $loop->index + 1 : $loop->index + 1 }}</td>
       <td class="text-center"><a href="{{ $wagon->viewPath() }}">{{ $wagon->inw }}</a></td>
+      <td class="text-right">{{ $wagon->detained_at->diffInDays() }}</td>
       <td class="text-right">{{ $wagon->detainedInHours() }}</td>
       <td>{{ $wagon->reason }}</td>
     </tr>
