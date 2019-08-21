bfcab5afe731e9f9f6c18b39e3ebb4caa08d8057
Ian Travers <mr.itpe@gmail.com>
2019-08-21 10:01:22 +0300

    Some russian translation

--- resources/views/partials/top-navbar.blade.php
+++ resources/views/partials/top-navbar.blade.php
@@ -38,14 +38,17 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav ml-auto">
         <!-- Authentication Links -->
+
         @guest
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            <a class="nav-link" href="{{ route('login') }}">Вход</a>
           </li>
+
           @if (Route::has('register'))
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+              <a class="nav-link" href="{{ route('register') }}">Регистрация</a>
             </li>
+
           @endif
         @else
           <li class="nav-item dropdown">
@@ -56,17 +59,18 @@
 
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="{{ route('logout') }}"
-                 onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Выход
               </a>
 
               <form id="logout-form" action="{{ route('logout') }}" method="POST"
                     style="display: none;">
+
                 @csrf
               </form>
             </div>
           </li>
+
         @endguest
       </ul>
     </div>
