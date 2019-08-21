594f96f93560dcb155bfffa8c812e60cdea860e1
Ian Travers <mr.itpe@gmail.com>
2019-08-21 10:39:16 +0300

    Custom report modal styles

--- resources/views/partials/custom-report-modal.blade.php
+++ resources/views/partials/custom-report-modal.blade.php
@@ -8,10 +8,44 @@
         </button>
       </div>
       <div class="modal-body">
-        <p>Форма выбора диапазона</p>
+        <h3 class="text-center mb-4">Выберите диапазон</h3>
+
+        <form action="">
+
+          @csrf
+          <div class="d-md-inline-flex justify-content-lg-start align-items-start">
+            <div class="form-group mr-2">
+              <label for="dtp_start" class="text-center d-block">Начало</label>
+              <div class="input-group" id="start" data-target-input="nearest">
+                <input type="text" id="dtp_start"
+                       name="start"
+                       class="form-control datetimepicker-input {{ $errors->has('start') ? 'is-invalid' : '' }}"
+                       data-target="#start"
+                />
+                <div class="input-group-append" data-target="#start" data-toggle="datetimepicker">
+                  <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group mr-2">
+              <label for="dtp_end" class="text-center d-block">Окончание</label>
+              <div class="input-group" id="end" data-target-input="nearest">
+                <input type="text" id="dtp_end"
+                       name="end"
+                       class="form-control datetimepicker-input {{ $errors->has('end') ? 'is-invalid' : '' }}"
+                       data-target="#end"
+                />
+                <div class="input-group-append" data-target="#end" data-toggle="datetimepicker">
+                  <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
       <div class="d-flex justify-content-between align-items-end p-3 border-top">
-        <button type="button" class="btn btn-lg btn-outline-primary">Сформировать отчет</button>
+        <button type="button" class="btn btn-lg btn-outline-primary mt-3">Сформировать отчет</button>
         <button type="button" class="btn btn-sm btn-outline-secondary" data-dismiss="modal">Отмена</button>
       </div>
     </div>
