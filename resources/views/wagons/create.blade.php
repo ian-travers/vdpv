9fb9b32c27f9bfa34a2c6dd0164923e12f95bb21
Ian Travers <mr.itpe@gmail.com>
2019-08-30 12:57:40 +0300

    Add default taken_measure

--- resources/views/wagons/create.blade.php
+++ resources/views/wagons/create.blade.php
@@ -5,7 +5,7 @@
     <h2>Добавление вагона</h2>
     <form action="{{ route('wagons.store') }}" method="post">
 
-      @include('wagons._form', [$wagon = new App\Wagon()])
+      @include('wagons._form', [$wagon = new App\Wagon(['taken_measure' => 'В ожидании ремонта. Дана телеграмма №'])])
     </form>
   </div>
 @endsection
