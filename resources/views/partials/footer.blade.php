e53f00d0e2345260bd6514968d99803853d3cd2d
Ian Travers <mr.itpe@gmail.com>
2019-10-02 10:49:49 +0300

    Update help

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="p-3 bg-dark d-flex justify-content-between align-items-end border-top border-warning">
   <div>
     <small class="text-light">&copy; 2019 ИВЦ Витебск.</small>
-    <small class="text-warning">Учет задержанных и длительно простаивающих вагонов на станции.</small>
+    <small class="text-warning">Учет задержанных и длительно простаивающих вагонов на станции Полоцк.</small>
   </div>
   <div>
     <small class="text-warning">По вопросам работы web-приложения обращаться по тел. 73-28-23.</small>
